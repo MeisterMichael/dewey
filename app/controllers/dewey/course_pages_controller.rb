5bdd2540998f55b11045d39cd01cb7dec11c488f
Michael Ferguson <meister@spacekace.com>
2019-02-11 20:14:48 -0800

    course pages

---
+++ app/controllers/dewey/course_pages_controller.rb
@@ -0,0 +1,17 @@
+module Dewey
+
+	class CoursePagesController < RootController
+		include Dewey::Concerns::DeweyConcern
+
+		def pulitzer_render(media)
+			
+			@course = media.root.course
+			@enrollment = current_user.enrollments.find( params[:enrollment_id] )
+			@previous_course_pages = @course.course_page.descendants.publish_at_before_now.active.where.not( lft: @media.left ).where( lft: 0..@media.left ).order(lft: :desc)
+			@next_course_pages = @course.course_page.descendants.publish_at_before_now.active.where.not( lft: @media.left ).where.not( lft: 0..@media.left ).order(lft: :asc)
+
+			super(media)
+		end
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Dewey::Engine.routes.draw do
 		delete :empty_trash, on: :collection
 	end
 	resources :course_cohort_admin
-	resources :courses, path: Dewey.courses_path
+	resources :courses, path: Dewey.courses_path do
+		resources :course_pages, path: 'pages'
+	end
 	resources :course_content_admin
-	resources :enrollments, path: Dewey.enrollments_path
+	resources :enrollments, path: Dewey.enrollments_path do
+		resources :course_pages, path: 'pages'
+	end
 
 end
