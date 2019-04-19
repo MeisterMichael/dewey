4628a32172bf536e9bc16bb5afc17ca902214342
Michael Ferguson <meister@spacekace.com>
2019-04-19 09:00:29 -0700

    log events

--- app/controllers/dewey/courses_controller.rb
+++ app/controllers/dewey/courses_controller.rb
@@ -6,6 +6,7 @@ module Dewey
 			def index
 				@courses = Dewey::Course.published
 				@courses = @courses.order( publish_at: :desc ).page(params[:page]).per(10)
+				log_event( { name: 'pageview' } )
 				set_page_meta( page_title: 'Courses' )
 			end
 
@@ -15,6 +16,8 @@ module Dewey
 				@enrollment = current_user.enrollments.where( course_cohort: Dewey::CourseCohort.where( course: @course ) ).last if current_user.present?
 
 				authorize( @course )
+
+				log_event( { name: 'pageview', on: @course } )
 
 				set_page_meta( @course.page_meta )
 				# render @course_content.template, layout: @course_content.layout
--- app/controllers/dewey/enrollments_controller.rb
+++ app/controllers/dewey/enrollments_controller.rb
@@ -12,6 +12,8 @@ module Dewey
 				authorize( @enrollment )
 
 				if @enrollment.save
+					log_event( { name: 'enrolled', category: 'dewey', on: @enrollment.course, content: "enrolled in #{@enrollment.course.title}." } )
+
 					set_flash "Congratulations you are enrolled!", :success
 					redirect_to enrollment_path( @enrollment )
 				else
@@ -24,6 +26,7 @@ module Dewey
 				@enrollments = current_user.enrollments.order( id: :desc )
 				@enrollments = @enrollments.page( params[:page] ).per(20)
 
+				log_event( { name: 'pageview' } )
 				set_page_meta( title: 'Enrollments' )
 			end
 
@@ -31,6 +34,8 @@ module Dewey
 				@course = Dewey::Course.find( params[:course_id] )
 				@course_cohort = @course.course_cohorts.open_for_enrollment.order( id: :desc ).first
 				@enrollment = Dewey::Enrollment.new( user: current_user, course_cohort: @course_cohort )
+
+				log_event( { name: 'pageview', on: @course } )
 
 				set_page_meta( @course.page_meta )
 			end
@@ -40,6 +45,8 @@ module Dewey
 				@course = @enrollment.course
 				@previous_course_pages = Dewey::CoursePage.none
 				@next_course_pages = @course.course_page.descendants.publish_at_before_now.active.order(lft: :asc)
+
+				log_event( { name: 'pageview', on: @course } )
 
 				set_page_meta( @course.page_meta )
 			end
